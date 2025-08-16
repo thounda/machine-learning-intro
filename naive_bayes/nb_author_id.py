b511f8cddfc5bbe1d2eca38300c3b99330618f88
Thounda Craig <thoundac94@gmail.com>
2025-08-16 19:11:00 -0400

    added initial note for Naive Bayes project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" 
+'''
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
     Use a Naive Bayes Classifier to identify emails by their authors
@@ -8,7 +8,29 @@
     authors and labels:
     Sara has label 0
     Chris has label 1
-"""
+
+INSTRUCTIONS
+To create and train a Naive Bayes classifier in your naive_bayes/nb_author_id.py file, you'll typically follow these steps:
+
+Import Necessary Libraries: Make sure to import the required libraries, such as numpy, sklearn.naive_bayes, and any other libraries you need for data handling.
+
+Load the Data: Load your training and test datasets. This is usually done using a function that preprocesses the email data.
+
+Create the Classifier: Instantiate the Naive Bayes classifier.
+
+Train the Classifier: Fit the classifier on your training data.
+
+Make Predictions: Use the trained classifier to make predictions on the test set.
+
+Calculate Accuracy: Compare the predicted labels with the actual labels to calculate accuracy.
+
+TIP
+1 Ensure that you have enough memory available, as some students have encountered memory issues. If you do face memory errors, consider setting test_size = 0.5 in your email_preprocess.py file, as mentioned in the course material.
+
+2 You can also explore different types of Naive Bayes classifiers, such as MultinomialNB or BernoulliNB, depending on your data characteristics.
+
+'''
+
     
 import sys
 from time import time
@@ -19,11 +41,23 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
-features_train, features_test, labels_train, labels_test = preprocess()
-
 
 ##############################################################
 # Enter Your Code Here
+
+### Load the data
+features_train, features_test, labels_train, labels_test = preprocess()
+
+# Create the classifier
+
+
+# Train the classifier
+
+
+# Make predictions
+
+
+# Calculate accuracy
 
 
 
